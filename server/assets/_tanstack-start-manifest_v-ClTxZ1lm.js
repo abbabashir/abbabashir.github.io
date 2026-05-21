b3181708332755cb2f99691011b9520d10052630
abbabashir <abbabashir516@gmail.com>
2026-05-21 14:29:47 -0600

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-ClTxZ1lm.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/nicho/Downloads/abbabashir/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BzEOiv6y.js"] }, "/": { filePath: "C:/Users/nicho/Downloads/abbabashir/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CgXt1H53.js"] } }, clientEntry: "/assets/index-BzEOiv6y.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-spr5m9Uk.js";
+import "node:events";
+export {
+  w as default
+};
